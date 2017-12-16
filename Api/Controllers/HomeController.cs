6b2163f0e2464f0d4beeebe10f404d5030991cdb
Mattias <github-ejtu5oeiughpiun4p@email.flowertwig.org>
2017-12-17 00:05:46 +0100

    if we visit root on api, redirect to login page for website
    mostly for when we deploy new version to azure

---
+++ Api/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers
+{
+    [Route("/")]
+    public class HomeController : Controller
+    {
+        // GET api/values
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return this.Redirect("https://polisensvolontarer.azurewebsites.net/login/");
+        }
+    }
+}
